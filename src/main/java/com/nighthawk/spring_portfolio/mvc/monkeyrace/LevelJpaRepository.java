ead2147d9e45601e57906fc21d75f3b30631f623
Rohan Juneja <rohanj2006@gmail.com>
2023-04-03 09:07:18 -0700

    level jpa repo fix

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/LevelJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/LevelJpaRepository.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LevelJpaRepository extends JpaRepository<Stage, Long> {
-    List<Stage> findByNumberAndStage(int number, Stage stage);
-    Stage findById(long id);
-    List<Stage> findByStage(Stage stage);
+public interface LevelJpaRepository extends JpaRepository<Level, Long> {
+    List<Level> findByNumberAndStage(int number, Stage stage);
+    Level findById(long id);
+    List<Level> findByStage(Stage stage);
 }
