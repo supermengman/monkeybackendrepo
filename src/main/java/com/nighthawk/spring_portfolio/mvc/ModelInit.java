31b512063f3ca09a54b77e48c2c9edb266e42175
supermengman <andrewm17us@gmail.com>
2023-04-18 22:57:43 -0700

    actual changes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -25,6 +25,8 @@ public class ModelInit {
                 new Level(null, "Level 1", 1, Problem.FRQ_B_2018.getName()),
                 new Level(null, "Level 2", 2, Problem.FRQ_4A_2017.getName()),
                 new Level(null, "Level 3", 3, Problem.FRQ_4B_2017.getName()),
+                new Level(null, "Level 4", 4, Problem.FRQ_A_2016.getName()),
+                new Level(null, "Level 5", 5, Problem.FRQ_B_2016.getName()),
                 new Level(null, "Completion", Level.DUMMY_LEVEL, null),
             };
 
