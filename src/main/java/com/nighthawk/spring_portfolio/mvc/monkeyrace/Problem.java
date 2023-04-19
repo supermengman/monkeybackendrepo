b373ad0c8f97a7df9845df504550440a6d7ce47d
supermengman <andrewm17us@gmail.com>
2023-04-18 22:57:05 -0700

    enum

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/Problem.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/Problem.java
@@ -1,7 +1,7 @@
 package com.nighthawk.spring_portfolio.mvc.monkeyrace;
 
 public enum Problem {
-    FRQ_A_2018("FRQ_A_2018"), FRQ_B_2018("FRQ_B_2018"), FRQ_4A_2017("FRQ_4A_2017"), FRQ_4B_2017("FRQ_4B_2017");
+    FRQ_A_2018("FRQ_A_2018"), FRQ_B_2018("FRQ_B_2018"), FRQ_4A_2017("FRQ_4A_2017"), FRQ_4B_2017("FRQ_4B_2017"), FRQ_A_2016("FRQ_A_2016"), FRQ_B_2016("FRQ_B_2016");
 
     private String name;
     private Problem (String name) {
