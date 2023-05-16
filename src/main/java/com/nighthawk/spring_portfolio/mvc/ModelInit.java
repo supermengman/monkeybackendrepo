ccf92b1b74de53686a0754c397c60c463692dc35
supermengman <andrewm17us@gmail.com>
2023-05-16 09:27:12 -0700

    hard code for now

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import com.nighthawk.spring_portfolio.mvc.monkeyrace.Problem;
 import com.nighthawk.spring_portfolio.mvc.monkeyrace.jpa.Level;
 import com.nighthawk.spring_portfolio.mvc.monkeyrace.jpa.LevelJpaRepository;
 
@@ -21,13 +20,13 @@ public class ModelInit {
         return args -> {
 
             Level[] levels = {
-                new Level(null, "Level 0", 0, Problem.FRQ_A_2018.getName()),
-                new Level(null, "Level 1", 1, Problem.FRQ_B_2018.getName()),
-                new Level(null, "Level 2", 2, Problem.FRQ_4A_2017.getName()),
-                new Level(null, "Level 3", 3, Problem.FRQ_4B_2017.getName()),
-                new Level(null, "Level 4", 4, Problem.FRQ_A_2016.getName()),
-                new Level(null, "Level 5", 5, Problem.FRQ_B_2016.getName()),
-                new Level(null, "Level 6", 6, Problem.Extra_Credit_Arrays.getName()),
+                new Level(null, "Level 0", 0, "2018FRQA.javat"),
+                new Level(null, "Level 1", 1, "2018FRQB.javat"),
+                new Level(null, "Level 2", 2, "2017FRQ4A.javat"),
+                new Level(null, "Level 3", 3, "2017FRQ4A.javat"),
+                new Level(null, "Level 4", 4, "2016FRQA.javat"),
+                new Level(null, "Level 5", 5, "2016FRQA.javat"),
+                new Level(null, "Level 6", 6, "ExtraCreditArrays.javat"),
                 new Level(null, "Completion", Level.DUMMY_LEVEL, null),
             };
 
