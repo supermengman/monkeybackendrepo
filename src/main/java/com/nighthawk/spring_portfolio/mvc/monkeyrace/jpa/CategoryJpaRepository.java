f59e439b8bacd4147aced204c1fd980cae989f9e
Rohan Juneja <rohanj2006@gmail.com>
2023-05-18 08:57:34 -0700

    jpa repo for category

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/CategoryJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/CategoryJpaRepository.java
@@ -1,5 +1,9 @@
 package com.nighthawk.spring_portfolio.mvc.monkeyrace.jpa;
 
-public class CategoryJpaRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
