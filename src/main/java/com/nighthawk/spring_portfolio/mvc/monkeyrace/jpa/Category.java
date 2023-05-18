4ba9a235c83a872f982ecfa9dc6e4b47eee8ac7d
Rohan Juneja <rohanj2006@gmail.com>
2023-05-18 08:55:46 -0700

    category pojo

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/Category.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/Category.java
@@ -1,5 +1,28 @@
 package com.nighthawk.spring_portfolio.mvc.monkeyrace.jpa;
 
+import java.util.stream.Stream;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Category {
-    
+   // id
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+
+    String name;
 }
