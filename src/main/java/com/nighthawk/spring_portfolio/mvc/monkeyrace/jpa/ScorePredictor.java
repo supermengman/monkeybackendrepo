ee3da00d29c1e627469ed25d11f95f7089b4af05
aaravarora01 <aaravarora09@gmail.com>
2023-05-30 09:33:40 -0700

    sorry rohan

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/ScorePredictor.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/jpa/ScorePredictor.java
@@ -20,20 +20,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public class ScorePredictor {
-    ResponseEntity<Object> response = CodeSnippet.getLevelList("your_jwt_value");
-
-    if (response.getStatusCode() == HttpStatus.OK) {
-        Object responseBody = response.getBody();
-        if (responseBody instanceof Map) {
-            Map<String, Object> result = (Map<String, Object>) responseBody;
-            List<Level> levels = (List<Level>) result.get("levels");
-            HashMap<Integer, Integer> status = (HashMap<Integer, Integer>) result.get("status");
-
-            int score1 = status.get(1);  // Get score for level 1
-            int score2 = status.get(2);  // Get score for level 2
-            int score3 = status.get(3);  // Get score for level 3
-        }
     
-        int predictedScore = runPythonScript(int score1, int score2, int score3);
-    }
+    
+        //int predictedScore = runPythonScript(int score1, int score2, int score3);
 }
