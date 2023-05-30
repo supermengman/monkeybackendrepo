4085fc57c6e49fb8629b2fb51c4f3c1198fd4298
aaravarora01 <aaravarora09@gmail.com>
2023-05-30 12:39:26 -0700

    delete

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/ScorePredictorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/ScorePredictorApiController.java
@@ -11,9 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
