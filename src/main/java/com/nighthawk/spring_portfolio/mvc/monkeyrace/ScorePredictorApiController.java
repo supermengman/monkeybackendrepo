5f1df0d34f9c697fbc5f8c09ce34f302e6cebaa4
aaravarora01 <aaravarora09@gmail.com>
2023-05-30 14:14:56 -0700

    delete extra imports

--- src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/ScorePredictorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/monkeyrace/ScorePredictorApiController.java
@@ -11,11 +11,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
